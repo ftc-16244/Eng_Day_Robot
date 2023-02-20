ccf400137400b9655010f1219a8c40bab142d8bf
atkindc <david.atkinsin@netzero.com>
2023-02-19 18:02:29 -0600

    old code new SDK

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enums/ShooterState.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Enums;
+
+public enum ShooterState {
+    STATE_SHOOTER_OFF,
+    STATE_SHOOTER_READY,
+    STATE_SHOOTER_ACTIVE;
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enums/WobbleLiftPosn.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Enums;
+
+public enum WobbleLiftPosn {
+    DOWN,
+    UP,
+    IDLE,
+
+}
